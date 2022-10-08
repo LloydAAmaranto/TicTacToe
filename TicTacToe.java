f500750b67dc8af91ce38443cd7167c7aef25e64
Lloyd Amaranto <lloydamaranto@MacBook-Pro-88.local>
2022-10-08 18:00:05 -0400

    Added initial declarations

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,6 +5,14 @@ import javax.swing.*;
 
 public class TicTacToe implements ActionListener{
 
+  Random random = new Random();
+  JFrame frame = new JFrame();
+  JPanel title_panel = new JPanel();
+  JPanel button_panel = new JPanel();
+  JLabel textfield = new JLabel();
+  JButton[] buttons = new JButton[9];
+  boolean player1_turn;
+
   public TicTacToe(){
 
   }
